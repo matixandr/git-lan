99372168d9af43f3bdd44a023b5d800e72db966d
matixandr <matixandr09@gmail.com>
2026-06-26 12:42:21 +0200

    cli: shell completion command (bash/zsh/fish/powershell)

---
+++ cmd/completion.go
@@ -0,0 +1,45 @@
+package cmd
+
+import (
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+var completionCmd = &cobra.Command{
+	Use:       "completion [bash|zsh|fish|powershell]",
+	Short:     "Generate shell completion script",
+	Long: `Generate a shell completion script for git-lan.
+
+Bash:
+  source <(git lan completion bash)
+
+Zsh:
+  git lan completion zsh > "${fpath[1]}/_git-lan"
+
+Fish:
+  git lan completion fish | source
+
+PowerShell:
+  git lan completion powershell | Out-String | Invoke-Expression`,
+	Args:                  cobra.ExactValidArgs(1),
+	ValidArgs:             []string{"bash", "zsh", "fish", "powershell"},
+	DisableFlagsInUseLine: true,
+	RunE: func(cmd *cobra.Command, args []string) error {
+		switch args[0] {
+		case "bash":
+			return rootCmd.GenBashCompletionV2(os.Stdout, true)
+		case "zsh":
+			return rootCmd.GenZshCompletion(os.Stdout)
+		case "fish":
+			return rootCmd.GenFishCompletion(os.Stdout, true)
+		case "powershell":
+			return rootCmd.GenPowerShellCompletionWithDesc(os.Stdout)
+		}
+		return nil
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(completionCmd)
+}
