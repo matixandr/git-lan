316a6f828623ba299034527015fb7c5262ecd8db
matixandr <matixandr09@gmail.com>
2026-06-26 12:43:29 +0200

    cli: git lan push with conflict-warning seam

---
+++ cmd/push.go
@@ -0,0 +1,60 @@
+package cmd
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/matixandr/git-lan/internal/display"
+	"github.com/matixandr/git-lan/internal/git"
+	"github.com/spf13/cobra"
+)
+
+var pushCmd = &cobra.Command{
+	Use:   "push <peer>[/repo] [branch]",
+	Short: "Push the current repository to a peer (peer must approve)",
+	Args:  cobra.RangeArgs(1, 2),
+	RunE: func(cmd *cobra.Command, args []string) error {
+		repo, err := git.Open("")
+		if err != nil {
+			return err
+		}
+		peer, remoteRepo, err := resolvePeer(args[0], 3*time.Second)
+		if err != nil {
+			return err
+		}
+		branch := ""
+		if len(args) == 2 {
+			branch = args[1]
+		}
+
+		// Warn about overlapping dirty files before sending anything (a fuller
+		// conflict check arrives with the conflict detector).
+		warnOnConflicts(cmd, repo, peer)
+
+		client, err := clientForPeer(peer)
+		if err != nil {
+			return err
+		}
+		url, stop, err := client.Bridge(remoteRepo)
+		if err != nil {
+			return err
+		}
+		defer stop()
+
+		fmt.Fprintf(cmd.OutOrStdout(), "%s pushing to %s/%s ...\n",
+			display.Icons.Branch, peer.Instance, remoteRepo)
+		if err := repo.Push(url, branch, cmd.OutOrStderr()); err != nil {
+			return err
+		}
+		fmt.Fprintf(cmd.OutOrStdout(), "%s pushed.\n", display.Icons.Success)
+		return nil
+	},
+}
+
+// warnOnConflicts is a placeholder hooked up fully by the conflict detector in
+// a later pass; for now it is a no-op seam so push has a stable call site.
+func warnOnConflicts(cmd *cobra.Command, repo *git.Repo, peer interface{}) {}
+
+func init() {
+	rootCmd.AddCommand(pushCmd)
+}
